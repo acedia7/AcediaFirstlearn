faa2d9fb0ac7dcf7604f6ad22ac6cac73f0c6324
acedia7 <1335357913@qq.com>
2023-12-06 17:20:06 +0800

    Merge remote-tracking branch 'origin/main'

--- mnist/My_Test.py
+++ mnist/My_Test.py
@@ -7,12 +7,8 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-#  2. 数据集准备：训练集、测试集：可在这时进行数据预处理，包括归一化、随机旋转、随机裁剪等
-'''
-Pytorch自己内置了一些入门数据集：包括Mnist、Cifar10等，直接导入即可
-'''
 transform = transforms.Compose([
-    transforms.ToTensor(),  # 可将PIL类型的图像或者Numpy数组转为Tensor，并将其像素值归一化到[0,1]
+    transforms.ToTensor(),
 ])
 
 # 训练集、测试集
@@ -67,26 +63,18 @@ class DemoModel(nn.Module):
         return self.model(x)
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # 确定采用cpu还是gpu计算
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = DemoModel()
-model = model.to(device)  # gpu加速
-# img,label=next(iter(train_dataloader))
-# # out=model(img)
-# # print(out.shape)
-# # print(model)
-# writer=SummaryWriter('./logs')
-# writer.add_graph(model,img)
-# writer.close()
-# 4.训练目标：损失函数
-creation = nn.CrossEntropyLoss()  # 损失函数：分类任务常用交叉熵函数，二分类任务可用nn.BCELoss()
+model = model.to(device)
+creation = nn.CrossEntropyLoss()
 creation = creation.to(device)
 
 # 5.优化器
 learning_rate = 1e-2
 beta1 = 0.9
 beta2 = 0.999
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(beta1, beta2))  # 优化器
-lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)  # 学习率衰减：余弦退火策略
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(beta1, beta2))
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=32)
 
 # 6.网络训练
 model_save_path = './models/'
@@ -97,20 +85,19 @@ train_steps = 0
 test_steps = 0
 for epoch in range(EPOCHS):  # 0-99
     print("第{}轮训练过程：".format(epoch + 1))
-    # 训练步骤
-    model.train()  # 训练模式
+
+    model.train()
     epoch_train_loss = 0.0
     for train_batch, (train_image, train_label) in enumerate(train_dataloader):
         train_image, train_label = train_image.to(device), train_label.to(device)  # 将数据送到GPU
         train_predictions = model(train_image)
         batch_train_loss = creation(train_predictions, train_label)
-        optimizer.zero_grad()  # 梯度清零
+        optimizer.zero_grad()
         batch_train_loss.backward()
         optimizer.step()
         epoch_train_loss += batch_train_loss.item()
         train_steps += 1
 
-    # 测试步骤
     model.eval()
     epoch_test_loss = 0.0
     epoch_test_acc = 0.0
@@ -121,7 +108,6 @@ for epoch in range(EPOCHS):  # 0-99
             test_loss = creation(predictions, test_label)
             epoch_test_loss += test_loss.item()
             test_steps += 1
-            # 计算每个批次数据测试时的准确率
             batch_test_acc = (predictions.argmax(dim=1) == test_label).sum()
             epoch_test_acc += batch_test_acc
 
